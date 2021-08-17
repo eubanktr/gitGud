1a058342866b99049df130e0ee03bfa57ed709e3
CarsonPC <carsoncoyle2015@gmail.com>
2021-08-17 15:22:13 -0400

    testing files

--- models/index.js
+++ models/index.js
@@ -3,23 +3,23 @@ const Systems = require('./Systems')
 const Ratings = require('./Ratings')
 const Users = require('./Users')
 
-Ratings.belongsTo(Games, {
-    foreignKey: 'games_id'
-})
+// Ratings.belongsTo(Games, {
+//     foreignKey: 'games_id'
+// })
 
-Users.hasMany(Systems, {
-    foreignKey: 'systems_id'
-})
+// Users.hasMany(Systems, {
+//     foreignKey: 'systems_id'
+// })
 
-Users.hasMany(Games, {
-    foreignKey: 'games_id'
-})
-Games.belongsTo(Users, {
-    foreignKey: 'users_id'
-})
+// Users.hasMany(Games, {
+//     foreignKey: 'games_id'
+// })
+// Games.belongsTo(Users, {
+//     foreignKey: 'users_id'
+// })
 
-Games.belongsTo(Systems, {
-    foreignKey: 'systems_id'
-})
+// Games.belongsTo(Systems, {
+//     foreignKey: 'systems_id'
+// })
 
 module.exports = { Games, Systems, Ratings, Users }
