68240a8aa7eb83105b70b3ec685ffe8bf7c21593
CarsonPC <carsoncoyle2015@gmail.com>
2021-08-18 13:02:22 -0400

    little fixes

--- seeds/games-seeds.js
+++ seeds/games-seeds.js
@@ -2,16 +2,20 @@ const { Games } = require('../models')
 
 const gameData = [
   {
-    game_title: 'Apex Legends'
+    game_title: 'Apex Legends',
+    ratings_id: 3
   },
   {
-    game_title: 'Call of Duty Warzone'
+    game_title: 'Call of Duty Warzone',
+    ratings_id: 4
   },
   {
-    game_title: 'Fortnite'
+    game_title: 'Fortnite',
+    ratings_id: 3
   },
   {
-    game_title: 'Rainbow Six: Siege'
+    game_title: 'Rainbow Six: Siege',
+    ratings_id: 3
   }
 ]
 
