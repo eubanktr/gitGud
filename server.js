75b213c66380ed4669a6d732a4edf86ceda42817
Thomas Eubank <eubanktr@gmail.com>
2021-08-18 14:09:13 -0400

    changes were made

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const bcrypt = require('bcrypt');
 const db = require('./models')
-// const routes = require('./controllers');
+const routes = require('./controllers');
 // const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
@@ -15,19 +16,19 @@ const PORT = process.env.PORT || 3001;
 // const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.SESS_PW, 
   cookie: {},
   resave: false,
   saveUnititialized: true,
-  store: new SequilizeStore({
+  store: new SequelizeStore({
     db: sequelize
   })
 };
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
 
 
 sequelize.sync({ force: true }).then(() => {
