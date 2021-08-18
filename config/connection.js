31bbbaa9b7d3311d42e82e4a92d701a44ea7faa5
Thomas Eubank <eubanktr@gmail.com>
2021-08-18 14:33:41 -0400

    tested put route

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
-
+require('dotenv').config();
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
