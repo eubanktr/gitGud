7eaca2c88f446a62564cc8d3bfac4d6ee1d962bb
Thomas Eubank <eubanktr@gmail.com>
2021-08-18 13:35:29 -0400

    round 2

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,4 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-
-const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
-
-module.exports = router;
-=======
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes')
 
@@ -15,4 +7,3 @@ router.use('/homeRoutes', homeRoutes);
 
 module.exports = router; 
 
->>>>>>> e50ca5c480cac456f2ec23e53362f9a6b303cebe
