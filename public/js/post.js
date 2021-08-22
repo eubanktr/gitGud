109f2668f0973c4053927dc9889afd5ca7ea72a9
Thomas Eubank <eubanktr@gmail.com>
2021-08-22 17:18:32 -0400

    updated get route for postspage. Populated database posts in handlebars

--- public/js/post.js
+++ public/js/post.js
@@ -5,20 +5,20 @@ const postForm = async (event) => {
   const game = document.getElementById('@@').value.trim();
   const body = document.getElementById('@@').value.trim();
 
-  // if (author && game && body) {
-  //   const response = await fetch(`/api/posts/`, {
-  //     method: 'POST',
-  //     body: JSON.stringify({ author, game, body}),
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //   });
-  //   if (response.ok) {
-  //     document.location.replace('/api/posts/')
-  //   } else {
-  //     alert('POST not uploaded' + response.statusText);
-  //   }
-  // }
+  if (author && game && body) {
+    const response = await fetch(`/api/posts/`, {
+      method: 'POST',
+      body: JSON.stringify({ author, game, body}),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      document.location.replace('/api/posts/')
+    } else {
+      alert('POST not uploaded' + response.statusText);
+    }
+  }
 };
 
 document.getElementById('post').addEventListener('click', postForm);
